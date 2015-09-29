47676f09591b950970687470e05fbf3bcc3ac945
Graeme Fulton <graemefulton89@gmail.com>
2015-09-29 22:30:51 +0100

    refactored list view into separate class

---
+++ MovieListScreen.js
@@ -0,0 +1,122 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+var {
+  Image,
+  ListView,
+  StyleSheet,
+  Text,
+  View
+} = React;
+
+var MovieListScreen = React.createClass({
+
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
+        });
+      })
+      .done();
+  },
+
+  render: function() {
+
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return this.renderListView();
+  },
+
+   renderLoadingView: function() {
+     return (
+       <View style={styles.container}>
+         <Text>
+           Loading movies...
+         </Text>
+       </View>
+     );
+   },
+
+   renderListView:function(){
+     return (
+       <ListView
+         dataSource={this.state.dataSource}
+         renderRow={this.renderMovie}
+         style={styles.listView}
+       />
+      );
+   },
+
+   renderMovie: function(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  },
+
+
+
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection:'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  rightContainer:{
+    flex: 1
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+module.exports = MovieListScreen;
--- index.android.js
+++ index.android.js
@@ -5,119 +5,20 @@
 'use strict';
 
 var React = require('react-native');
-
-/**
- * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
- * their very own API that lives in React Native's Github repo.
- */
-var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+var MovieListScreen = require('./MovieListScreen');
 
 var {
-  AppRegistry,
-  Image,
-  ListView,
-  StyleSheet,
-  Text,
-  View
+  AppRegistry
 } = React;
 
 var movieFilmz = React.createClass({
 
-  getInitialState: function() {
-    return {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      }),
-      loaded: false,
-    };
-  },
-
-  componentDidMount: function() {
-    this.fetchData();
-  },
-  fetchData: function() {
-    fetch(REQUEST_URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
-          loaded: true,
-        });
-      })
-      .done();
-  },
 
   render: function() {
-
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
-
-    return this.renderListView();
-  },
-
-   renderLoadingView: function() {
-     return (
-       <View style={styles.container}>
-         <Text>
-           Loading movies...
-         </Text>
-       </View>
-     );
-   },
-
-   renderListView:function(){
-     return (
-       <ListView
-         dataSource={this.state.dataSource}
-         renderRow={this.renderMovie}
-         style={styles.listView}
-       />
-      );
-   },
-
-   renderMovie: function(movie) {
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{uri: movie.posters.thumbnail}}
-          style={styles.thumbnail}
-        />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{movie.title}</Text>
-          <Text style={styles.year}>{movie.year}</Text>
-        </View>
-      </View>
-    );
+    return(<MovieListScreen />)
   },
 
 
-
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection:'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  rightContainer:{
-    flex: 1
-  },
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center',
-  },
-  year: {
-    textAlign: 'center',
-  },
 });
 
 AppRegistry.registerComponent('movieFilmz', () => movieFilmz);
